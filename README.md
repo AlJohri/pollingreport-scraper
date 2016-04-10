392b84dded47090bdbb37688d636d2cbe5eb665e
Al Johri <al.johri@gmail.com>
2016-04-10 18:02:51 -0400

    semi-scrape polls into csv before manually normalizing

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 Scrapes publically available polls from [polling report](http://www.pollingreport.com/). Specifically scrapes the poll pages labeled as "detailed" ([example](http://www.pollingreport.com/obama_job2.htm)).
 
-![](http://i.imgur.com/KRG8Uen.png)
+Because scraping Microsoft Frontpage can only be so accurate. An attempt to manually normalize the polls is here: https://docs.google.com/spreadsheets/d/1XZ_qSBlqFrsyhYzrHjljAjOHMInAMxwXlXS9b0YBVdg/edit?usp=sharing
+
+![](http://i.imgur.com/xrrIZfZ.png)
 
 ## Setup
 
-Requires Python 3.5+
+Requires Python 3.5+. [Anaconda](https://www.continuum.io/downloads) recommended due the numpy/pandas dependency.
 
 ```
 pip install -r requirements.txt
--- scrape_pollingreport.py
+++ scrape_pollingreport.py
@@ -1,10 +1,19 @@
-import os, re, sys, requests, lxml.html
+import os, re, sys, csv, requests, lxml.html
+from collections import defaultdict
+from pprint import pprint as pp
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.width', 1000)
 
 months = r"Jan\.|Feb\.|March|April|May|June|July|Aug\.|Sept\.|Oct\.|Nov\.|Dec\."
 
 same_month_date_range = re.compile(r"(%s) (\d+)-(\d+), (\d{4})\." % months)
 different_month_date_range = re.compile(r"(%s) (\d+)-(%s) (\d+), (\d{4})\." % (months, months))
 single_date = re.compile(r"(%s) (\d+), \d{4}\." % months)
+
+def is_date(x):
+    return bool(re.search(r"[\d-]+", x))
 
 from blessings import Terminal
 t = Terminal()
@@ -19,13 +28,18 @@ def get_first(el, selector):
 def get_stripped_text(el):
     return re.sub(r'\s+', ' ', el.text_content()).strip() if el is not None else ""
 
-def scrape_page(url):
+def scrape_page(url, f):
     response = requests.get(url)
     doc = lxml.html.fromstring(response.content)
+
+    question_counter = 0
 
     for separator in doc.cssselect("hr[size='1'][color='#C0C0C0']"):
         parents = separator.iterancestors(tag='table')
         poll = next(parents)
+
+        # Hack
+        if "President Obama: Job Ratings" in poll.text_content(): continue
 
         # poll_title_el = get_first(poll, "font[color='#004080']")
         # poll_title = get_stripped_text(poll_title_el)
@@ -60,16 +74,138 @@ def scrape_page(url):
 
         all_text = get_stripped_text(poll)
 
+        qindextitle = url.split("/")[-1] + " | Question %d" % question_counter
+
+        print(t.bold_white(qindextitle))
         print(t.bold_white(poll_title))
-        print([date, margin_of_error, sample_size, subpopulation])
-        print(question)
+        print(t.yellow(question))
+        print([margin_of_error, sample_size, subpopulation])
+        question_counter += 1
 
-        if any([keyword in all_text for keyword in favorability_keywords]):
-            print(t.yellow("Potentially Favorability"))
+        f.write("\"" + qindextitle + "\"" ); f.write("\n")
+        f.write("\""  + poll_title + "\"" ); f.write("\n")
+        f.write(question); f.write("\n")
 
-        if any([keyword in all_text for keyword in approval_keywords]):
-            print(t.yellow("Potentially Approval"))
+        # if any([keyword in all_text for keyword in favorability_keywords]):
+        #     print(t.yellow("Potentially Favorability"))
 
+        # if any([keyword in all_text for keyword in approval_keywords]):
+        #     print(t.yellow("Potentially Approval"))
+
+        # Find first row after question
+        question_row = next(question_el.iterancestors('tr'))
+        first_data_row = question_row.getnext()
+        if len(first_data_row.cssselect("td")) == 1:
+            first_data_row = first_data_row.getnext()
+
+        # TODO: parse poll properly if there is no line between question and first row
+        # e.g. last 2 polls on http://www.pollingreport.com/bushfav2.htm
+
+        # Find all rows after the question
+        rows = [first_data_row] + list(first_data_row.itersiblings())
+
+        # Remove last row if it contains an hr tag
+        if rows[-1].cssselect("hr"): del rows[-1]
+
+        num_columns = len(rows[0].cssselect("td"))
+
+        # Ensure that all rows have the same number of columns
+        if all([len(row.cssselect("td")) == num_columns for row in rows]):
+        # Filter down to rows with at least 4 columns (ideally the data)
+        # rows = [row for row in rows if len(row.cssselect("td")) > 4]
+
+            # Remove first row if its entirely blank or a single dot?
+            if re.search(r'^\s+|\.$', rows[0].text_content().strip()): del rows[0]
+
+            sample_size_column_index = None
+
+            # Remove second row if it has a percentage sign in it.
+            if re.search(r'%', rows[1].text_content()):
+                if re.search(r'N', rows[1].text_content()):
+                    sample_size_column_index = [i for i, x in  enumerate(rows[1].cssselect("td")) if get_stripped_text(x) == "N"][0]
+                del rows[1]
+
+            # First row should now be the header row
+            if re.search(r"Favorable|Unfavorable|Approve|Disapprove", rows[0].text_content()):
+                # continue parsing this table
+
+                headers = [get_stripped_text(x) for x in rows[0].cssselect("td")]
+
+                for i, header in enumerate(headers):
+                    if header == "Disap- prove":
+                        headers[i] = "Disapprove"
+
+                    if header == "Unfav- orable":
+                        headers[i] = "Unfavorable"
+
+                if sample_size_column_index is not None:
+                    headers[sample_size_column_index] = "sample_size"
+
+                data = []
+
+                for row in rows[1:]:
+                    actual_row = [get_stripped_text(x) for x in row]
+                    data.append(actual_row)
+
+                # DEBUG
+                # print(headers)
+                # pp(data)
+
+                df = pd.DataFrame(data, columns=headers)
+                df.replace('', np.nan, regex=True, inplace=True)
+
+                # Remove first column if its entirely blank
+                df.dropna(axis=1, how='all', inplace=True)
+
+                if is_date(df.iloc[0,0]):
+                    columns = list(df.columns)
+                    columns[0] = "Date"
+                    df.columns = columns
+
+                # TODO: parse out pollster from poll_title
+                # TODO: parse out subpopulation from "date"
+                # TODO: parse date into start date / end date
+
+                if 'sample_size' not in df:
+                    df['sample_size'] = np.nan
+                else:
+                    df['sample_size'] = df['sample_size'].replace(',', '', regex=True)
+
+                df['margin_of_error'] = np.nan
+                df['subpopulation'] = np.nan
+
+                if pd.isnull(df.ix[0, 'sample_size']):
+                    df.ix[0, 'sample_size'] = sample_size or np.nan
+
+                if pd.isnull(df.ix[0, 'margin_of_error']):
+                    df.ix[0, 'margin_of_error'] = margin_of_error or np.nan
+
+                if pd.isnull(df.ix[0, 'subpopulation']):
+                    df.ix[0, 'subpopulation'] = subpopulation or np.nan
+
+                print(t.green(str(df))) # .to_csv(index=False)
+
+                df['url'] = url
+                df['original'] = poll_title
+                df['question'] = question
+                df['Date'] = df['Date'].map(lambda x: "=\"" + x + "\"")
+
+                f.write(df.to_csv(index=False))
+                f.write("\n")
+        
+        else:
+            print(t.red("table could not be parsed. expecting all rows to have %d columns. offending rows:" % num_columns))
+            for row in rows:
+                if len(row.cssselect("td")) != len(rows[0].cssselect("td")):
+                    print("\t", [get_stripped_text(x) for x in row.cssselect("td")])
+
+            f.write("manually enter poll"); f.write("\n")
+
+        f.write("\n")
+        f.write("\n")
+        f.write("\n")
+        f.write("\n")
+        f.write("\n")
         print()
 
 favorability_keywords = ["favorable", "unfavorable"]
@@ -79,41 +215,43 @@ if __name__ == '__main__':
 
     urls = [
 
-        # Obama Job Ratings
-        # "http://www.pollingreport.com/obama_job1.htm", # Gallup Daily Tracking (don't use)
-        # "http://www.pollingreport.com/obama_job1a.htm", # Gallup Daily Tracking (don't use)
-        "http://www.pollingreport.com/obama_job2.htm",
-
-        # Bush Job Ratings
-        "http://www.pollingreport.com/BushJob1.htm",
-        "http://www.pollingreport.com/bushjob2.htm",
-        "http://www.pollingreport.com/bushjob3.htm"
-
-        # Clinton Job Ratings
-        "http://www.pollingreport.com/clinton-.htm",
-
-        # Favorability Specific Pages
-        "http://www.pollingreport.com/obama_fav.htm", # Obama
-        "http://www.pollingreport.com/BushFav.htm", # George W. Bush (page 1)
-        "http://www.pollingreport.com/bushfav2.htm", # George W. Bush (page 2)
+        # # Favorability Specific Pages
+        # "http://www.pollingreport.com/BushFav.htm", # George W. Bush (page 1)
+        # "http://www.pollingreport.com/bushfav2.htm", # George W. Bush (page 2)
         "http://www.pollingreport.com/clinton1.htm", # Bill Clinton
+        # "http://www.pollingreport.com/obama_fav.htm", # Obama
 
-        # Political Figure Pages
-        "http://www.pollingreport.com/A-B.htm", # includes George H. W. Bush
-        "http://www.pollingreport.com/c.htm", # includes Jimmy Carter
-        "http://www.pollingreport.com/hrc.htm", # Hillary Clinton
-        "http://www.pollingreport.com/d.htm",
-        "http://www.pollingreport.com/e-f.htm",
-        "http://www.pollingreport.com/g.htm",
-        "http://www.pollingreport.com/h-j.htm",
-        "http://www.pollingreport.com/k.htm",
-        "http://www.pollingreport.com/l.htm",
-        "http://www.pollingreport.com/o.htm",
-        "http://www.pollingreport.com/p.htm",
-        "http://www.pollingreport.com/r.htm",
-        "http://www.pollingreport.com/S-Z.htm"
+        # # Obama Job Ratings
+        # # "http://www.pollingreport.com/obama_job1.htm", # Gallup Daily Tracking (don't use)
+        # # "http://www.pollingreport.com/obama_job1a.htm", # Gallup Daily Tracking (don't use)
+        # "http://www.pollingreport.com/obama_job2.htm",
+
+        # # Bush Job Ratings
+        # "http://www.pollingreport.com/BushJob1.htm",
+        # "http://www.pollingreport.com/bushjob2.htm",
+        # "http://www.pollingreport.com/bushjob3.htm",
+
+        # # Clinton Job Ratings
+        # "http://www.pollingreport.com/clinton-.htm",
+
+        # # Political Figure Pages
+        # "http://www.pollingreport.com/A-B.htm", # includes George H. W. Bush
+        # "http://www.pollingreport.com/c.htm", # includes Jimmy Carter
+        # "http://www.pollingreport.com/hrc.htm", # Hillary Clinton
+        # "http://www.pollingreport.com/d.htm",
+        # "http://www.pollingreport.com/e-f.htm",
+        # "http://www.pollingreport.com/g.htm",
+        # "http://www.pollingreport.com/h-j.htm",
+        # "http://www.pollingreport.com/k.htm",
+        # "http://www.pollingreport.com/l.htm",
+        # "http://www.pollingreport.com/o.htm",
+        # "http://www.pollingreport.com/p.htm",
+        # "http://www.pollingreport.com/r.htm",
+        # "http://www.pollingreport.com/S-Z.htm"
+
     ]
 
-    for url in urls:
-        print(t.bold_magenta("Scraping {}".format(url)))
-        scrape_page(url)
+    with open("polls.csv", "w", encoding="latin-1") as f:
+        for url in urls:
+            print(t.bold_magenta("Scraping {}".format(url)))
+            scrape_page(url, f)
